c371d2995e6fe40edba5239c270c24e64784a006
grscheller <geoffrey@scheller.com>
2025-10-13 11:30:47 -0600

    Updated docstrings.
    - __init__,py
    - sentinels/__init__,py

--- src/pythonic_fp/gadgets/__init__.py
+++ src/pythonic_fp/gadgets/__init__.py
@@ -20,19 +20,21 @@ Library of simple, but useful, functions and classes with no external
 dependencies besides the those from the Python standard Library. This
 includes other Pythonic Functional Programming dependencies.
 
-+------------------------------+----------------------+------------------------------------------------+
-| Description                  | Gadget               | Module                                         |
-+==============================+======================+================================================+
-| Return Iterator of arguments | function ``ita``     | ``pythonic_fp.gadgets.iterate_arguments``      |
-+------------------------------+----------------------+------------------------------------------------+
-| Find least common base class | function ``lca``     | ``pythonic_fp.gadgets.latest_common_ancestor`` |
-+------------------------------+----------------------+------------------------------------------------+
-| Single item box              | class ``Box``        | ``pythonic_fp.gadgets.box``                    |
-+------------------------------+----------------------+------------------------------------------------+
-| Wrapped hashable item        | class ``Wrap``       | ``pythonic_fp.gadgets.wrap``                   |
-+------------------------------+----------------------+------------------------------------------------+
-| sentinels values with extras | module ``sentinals`` | ``pythonic_fp.gadgets.sentinels``              |
-+------------------------------+----------------------+------------------------------------------------+
++----------------------+------------------------------+------------------------------------+
+| Gadget               | Description                  | Module                             |
++======================+==============================+====================================+
+| function ``ita``     | Return Iterator of arguments | ``gadgets.iterate_arguments``      |
++----------------------+------------------------------+------------------------------------+
+| function ``lca``     | Find least common base class | ``gadgets.latest_common_ancestor`` |
++----------------------+------------------------------+------------------------------------+
+| class ``Box``        | Single item box              | ``gadgets.box``                    |
++----------------------+------------------------------+------------------------------------+
+| class ``Wrap``       | Wrapped item                 | ``gadgets.wrap``                   |
++----------------------+------------------------------+------------------------------------+
+| class ``HWrap``      | Wrapped hashable item        | ``gadgets.wrap``                   |
++----------------------+------------------------------+------------------------------------+
+| module ``sentinels`` | Sentinels values with extras | ``gadgets.sentinels``              |
++----------------------+------------------------------+------------------------------------+
 
 """
 
